0fb39bab9c19699baa27ac295f56104919a1cfa6
shivamdubeyagra <shivamdubeyagra@gmail.com>
2024-08-03 14:48:07 +0530

    optimized appraoch

--- LeetCode/128. Longest Consecutive Sequence.js
+++ LeetCode/128. Longest Consecutive Sequence.js
@@ -41,6 +41,34 @@ var longestConsecutive = function (nums) {
   longStreak = Math.max(currStreak, longStreak);
   return longStreak;
 };
+var longestConsecutive = function (nums) {
+  if (nums.length === 0) return 0;
 
-const nums = [0, 3, 7, 2, 5, 8, 4, 6, 0, 1];
+  const numSet = new Set(nums);
+  let longestStreak = 0;
+
+  for (let num of numSet) {
+    // Check if it's the start of a sequence
+    if (!numSet.has(num - 1)) {
+      let currentNum = num;
+      let currentStreak = 1;
+
+      while (numSet.has(currentNum + 1)) {
+        currentNum++;
+        currentStreak++;
+      }
+
+      longestStreak = Math.max(longestStreak, currentStreak);
+    }
+  }
+
+  return longestStreak;
+};
+
+const nums = [100, 4, 200, 1, 3, 2];
+console.log(longestConsecutive(nums)); // Expected output: 4
+
+// const nums = [0, 3, 7, 2, 5, 8, 4, 6, 0, 1];
 console.log(longestConsecutive(nums));
+// const mySet = new Set(nums);
+// console.log(mySet);
