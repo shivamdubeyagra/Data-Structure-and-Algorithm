0718efa7b8121362211c7fb2053282e4d298a356
shivamdubeyagra <shivamdubeyagra@gmail.com>
2024-08-26 09:08:16 +0530

    improved buddlesort

--- LeetCode/Bubble Sort.js
+++ LeetCode/Bubble Sort.js
@@ -1,10 +1,15 @@
 function bubbleSort(arr){
     let n = arr.length;
     for(let i=n-1; i>=0; i--){
+        let didSwap = 0
         for(let j=0; j<=i-1; j++){
             if(arr[j]>arr[j+1]){
                 [arr[j],arr[j+1]] = [arr[j+1],arr[j]]
+                didSwap =1
             }
+        }
+        if(!didSwap){
+            break;
         }
     }
     return arr
