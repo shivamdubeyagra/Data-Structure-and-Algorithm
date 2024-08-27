6fb6a896cb1b99a92e1c164ef0700eb1faf191eb
shivamdubeyagra <shivamdubeyagra@gmail.com>
2024-08-27 09:49:28 +0530

    Insertion Sort

--- LeetCode/Insertion Sort.js
+++ LeetCode/Insertion Sort.js
@@ -1,8 +1,22 @@
-function insertionSort(){
-
+function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let nti = arr[i];
+    let j = i - 1;
+    console.log(`outerLoop`, arr);
+    while (j >= 0 && arr[j] > nti) {
+      arr[j + 1] = arr[j];
+      console.log(`InnerLoop`, arr);
+      j--;
+    }
+    arr[j + 1] = nti;
+    console.log(`outerLoopB`, arr);
+    console.log(`one iter over ..................`);
+  }
+  return arr;
 }
 
-const arr = [14,9,15,12,6,8,13]
-console.log('Hello World');
+const arr = [14, 9, 15, 12, 6, 8, 13];
+// const arr = [-6, 20, 8, -2, 4];
+// console.log("Hello World");
 
 console.log(insertionSort(arr));
