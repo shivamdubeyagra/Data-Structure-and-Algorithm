ed3e547d0cc9bb8975c36395ab8507d7e64af254
shivamdubeyagra <shivamdubeyagra@gmail.com>
2024-08-30 21:35:44 +0530

    Hero

--- LeetCode/Insertion Sort.js
+++ LeetCode/Insertion Sort.js
@@ -1,22 +1,22 @@
 function insertionSort(arr) {
+  // Start from the second element since the first element is already sorted
   for (let i = 1; i < arr.length; i++) {
+    // Store the current element in a variable
     let nti = arr[i];
+    // Initialize j to be the index of the previous element
     let j = i - 1;
-    console.log(`outerLoop`, arr);
+    // Shift elements of the sorted portion of the array to the right
+    // until the correct position for nti is found
     while (j >= 0 && arr[j] > nti) {
-      arr[j + 1] = arr[j];
-      console.log(`InnerLoop`, arr);
+      arr[j + 1] = arr[j]; // Move element one position to the right
       j--;
     }
+    // Insert the current element into its correct position
     arr[j + 1] = nti;
-    console.log(`outerLoopB`, arr);
-    console.log(`one iter over ..................`);
   }
+  // Return the sorted array
   return arr;
 }
-
+// Example usage
 const arr = [14, 9, 15, 12, 6, 8, 13];
-// const arr = [-6, 20, 8, -2, 4];
-// console.log("Hello World");
-
-console.log(insertionSort(arr));
+console.log(insertionSort(arr)); // Output: [6, 8, 9, 12, 13, 14, 15]
--- LeetCode/index.js
+++ LeetCode/index.js
@@ -1,2 +1,4 @@
 console.log("hello world");
 console.log("Hello World");
+console.log('I am a Hero');
+
