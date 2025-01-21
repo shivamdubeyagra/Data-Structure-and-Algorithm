5e637f66c9506525922f9ea84e2638a32300291d
Shivam Dubey <121815825+shivamdubeyagra@users.noreply.github.com>
2025-01-21 23:44:32 +0530

    binarySearch Tree
    insert() done

---
+++ binaryTree.js
@@ -0,0 +1,46 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+  insert(value) {
+    const newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      let currentNode = this.root;
+      while (true) {
+        if (value < currentNode.value) {
+          if (!currentNode.left) {
+            currentNode.left = newNode;
+            return this;
+          }
+          currentNode = currentNode.left;
+        } else {
+          if (!currentNode.right) {
+            currentNode.right = newNode;
+            return this;
+          }
+          currentNode = currentNode.right;
+        }
+      }
+    }
+  }
+  lookup(value) {}
+  remove(value) {}
+}
+
+const tree = new BinarySearchTree();
+tree.insert(9);
+console.log(tree);
+tree.insert(4);
+console.log(tree);
+tree.insert(6);
+console.log(tree);
