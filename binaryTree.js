9b532648232b9e8f33f83d7f45b07ad1a2382f28
Shivam Dubey <121815825+shivamdubeyagra@users.noreply.github.com>
2025-01-22 23:10:38 +0530

    added lookup
    in the tree

--- binaryTree.js
+++ binaryTree.js
@@ -33,8 +33,46 @@ class BinarySearchTree {
       }
     }
   }
-  lookup(value) {}
-  remove(value) {}
+  lookup(value) {
+    if(!this.root){
+      return false;
+    }
+    let currentNode = this.root;
+    while(currentNode){
+      if(value < currentNode.value){
+        currentNode = currentNode.left;
+      }else if(value > currentNode.value){
+        currentNode = currentNode.right;
+      }else if(currentNode.value === value){
+        return currentNode;
+      }
+    }
+    return false;
+  }
+  remove(value) {
+    if(!this.root) return false;
+    let currentNode = this.root;
+    let parentNode = null;
+    while(currentNode){
+      if(value<currentNode.value){
+        parentNode = currentNode;
+        currentNode = currentNode.left
+      }else if(value > currentNode.value){
+        parentNode = currentNode;
+        currentNode = currentNode.right;
+      }else if(currentNode.value === value){
+        if(currentNode.right === null){
+          if(parentNode === null){
+            this.root = currentNode.left
+          }else{
+            if(currentNode.value < parentNode.value){
+              parentNode.left = currentNode.left;
+            }
+          }
+        }
+      }
+    }
+  }
 }
 
 const tree = new BinarySearchTree();
@@ -45,4 +83,5 @@ tree.insert(20);
 tree.insert(170);
 tree.insert(15);
 tree.insert(1);
+console.log(tree.lookup(4));
 console.log(tree);
