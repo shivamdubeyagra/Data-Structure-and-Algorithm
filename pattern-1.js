ed8fde428d185fdeb9c26e58698c1eaefc5b4d43
Shivam Dubey <shivamdubeyagra@gmail.com>
2025-07-30 09:38:10 +0530

    pattern -12

--- pattern-1.js
+++ pattern-1.js
@@ -78,6 +78,7 @@ for(let i = n - 1; i >= 1; i--) {
     console.log(spaces + stars);
 }
 */
+/*
 // pattern -11
 const n =5;
 for(let i=1; i<=n; i++){
@@ -89,3 +90,52 @@ for(let i=1; i<=n; i++){
     }
     console.log(x)
 }
+    */
+// pattern - 12
+/*
+const n = 10;
+let resutl ='';
+for(let i=1; i<=n; i++){
+    for(let j=1; j<=i; j++){
+        resutl+=j;
+    }
+    resutl +=" ".repeat((n-i)*2);
+    for(let j=i; j>=1; j--){
+        resutl+=j;
+    }
+    resutl+='\n';
+}
+console.log(resutl)
+*/
+const n = 30;
+let result = '';
+
+function getNumberWidth(num) {
+    return num.toString().length;
+}
+function getHalfWidth(maxNum) {
+    let width = 0;
+    for (let i = 1; i <= maxNum; i++) {
+        width += getNumberWidth(i);
+    }
+    return width;
+}
+
+const maxWidth = getHalfWidth(n) * 2; 
+
+for (let i = 1; i <= n; i++) {
+    let line = '';
+    for (let j = 1; j <= i; j++) {
+        line += j;
+    }
+    const currentHalfWidth = getHalfWidth(i);
+    const currentTotalWidth = currentHalfWidth * 2;
+    const spacesNeeded = maxWidth - currentTotalWidth;
+    line += " ".repeat(spacesNeeded);
+    for (let j = i; j >= 1; j--) {
+        line += j;
+    }
+    result += line + '\n';
+}
+
+console.log(result);
