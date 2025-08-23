a42f3442b3fd43cb6af5579139a883062b8af78f
Shivam Dubey <shivamdubeyagra@gmail.com>
2025-08-23 19:18:28 +0530

    Single Number optimal solution

--- LeetCode/136. Single Number.js
+++ LeetCode/136. Single Number.js
@@ -3,3 +3,9 @@ const nums = [4,1,2,1,2];
 const counts = new Map();
 for (const num of nums) counts.set(num, (counts.get(num) ?? 0) + 1);
 for (const [num, count] of counts) if (count === 1) console.log(num);
+
+let ans = 0;
+for(const x of nums){
+    ans ^= x;
+}
+console.log(ans)
