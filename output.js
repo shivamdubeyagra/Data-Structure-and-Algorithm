8522da40b9e4989c3d2222f5533c2bf76b410e8e
Shivam Dubey <shivamdubeyagra@gmail.com>
2025-08-25 09:56:03 +0530

    subarray positive

--- output.js
+++ output.js
@@ -1,4 +1,5 @@
 //136. Single Number
+/*
 const nums = [4,1,2,1,2];
 const counts = new Map();
 for (const num of nums) counts.set(num, (counts.get(num) ?? 0) + 1);
@@ -9,3 +10,57 @@ for(const x of nums){
     ans ^= x;
 }
 console.log(ans)
+*/
+
+//325. Maximum Size Subarray Sum Equals k
+
+function logestSubarraySumNegativePostive(arr,k){
+    const n = arr.length;
+    let maxLen = 0;
+    let results = [];
+
+    for(let i=0; i<n; i++){
+        let sum =0;
+        let temp = [];
+        for(let j=i; j<n; j++){
+            sum+=arr[j];
+            temp.push(arr[j]);
+            if(sum==k){
+                maxLen = Math.max(maxLen,j-i+1)
+                results.push(temp)
+            }
+        }
+    }
+    return {maxLen,results};
+}
+
+let arr = [1, 2, 3, 1, 1, 1, 1, 4, 2, 3];
+let K = 6;
+console.log(logestSubarraySumNegativePostive(arr, K));
+
+
+function longestSubarrayPositive(arr, k) {
+    const n = arr.length;
+    let sum = 0;
+    let maxLen = 0;
+    let i = 0;
+
+    for (let j = 0; j < n; j++) {
+        sum += arr[j];
+
+        // shrink window if sum > k
+        while (sum > k && i <= j) {
+            sum -= arr[i];
+            i++;
+        }
+
+        // check again after shrinking
+        if (sum === k) {
+            maxLen = Math.max(maxLen, j - i + 1);
+        }
+    }
+    return maxLen;
+}
+
+// Example
+console.log(longestSubarrayPositive([1,2,3,1,1,1,1,4,2,3], 6)); // Output: 4
