5095674f7c2e2fa7b102ac258642c0e7e035e14c
Shivam Dubey <shivamdubeyagra@gmail.com>
2025-09-04 09:29:15 +0530

    linked List

--- output.js
+++ output.js
@@ -423,7 +423,7 @@ var groupAnagrams = function (strs) {
 const strs = ["eat", "tea", "tan", "ate", "nat", "bat"];
 console.log(groupAnagrams(strs));
 */
-
+/*()
 var isPalindrome = function(s) {
     let i = 0, j = s.length - 1;
 
@@ -449,3 +449,111 @@ function isAlphaNumeric(ch) {
 }
 const s = "A man, a plan, a canal: Panama";
 console.log(isPalindrome(s));
+*/
+
+// Linked List 
+class Node {
+    constructor(value){
+        this.value = value;
+        this.next = null;
+    }
+}
+class LinkedList {
+    constructor(){
+        this.head = null;
+        this.size = 0;
+    }
+    isEmpty(){
+        return this.size === 0;
+    }
+    getSize(){
+        return this.size;
+    }
+    prepend(value){
+        const node = new Node(value);
+        if(this.isEmpty()){
+            this.head = node;
+        }else{
+            node.next = this.head;
+            this.head = node;
+        }
+        this.size++;
+        return this.head;
+    }
+    append(value){
+        const node = new Node(value);
+        if(this.isEmpty()){
+            this.head = node;
+        }else{
+            let curr = this.head;
+            while(curr.next){
+                curr = curr.next;
+            }
+            curr.next = node;
+        }
+        this.size++;
+        return this.head;
+    }
+    insert(value,index){
+        if(index < 0 || index > this.size){
+            return `Invalid index`;
+        }
+        if(index === 0){
+            this.prepend(value);
+        }else{
+            const node = new Node(value);
+            let curr = this.head;
+            for(let i=0; i<index-1; i++){
+                curr = curr.next;
+            }
+            node.next = curr.next;
+            curr.next = node;
+            this.size++;
+        }
+        return this.head;
+    }
+    print(){
+        if(this.isEmpty()){
+            return null;
+        }
+        let curr = this.head;
+        let listValues = '';
+        while(curr){
+            listValues += `${curr.value} `;
+            curr = curr.next;
+        }
+        console.log(listValues);
+    }
+};
+const linkedList = new LinkedList();
+console.log(linkedList.prepend(5))
+console.log(linkedList.prepend(10))
+console.log(linkedList.prepend(15))
+linkedList.print();
+console.log(linkedList.append(20))
+linkedList.print();
+console.log(linkedList.isEmpty());
+console.log(linkedList.getSize());
+console.log(linkedList.insert(25,2))
+linkedList.print();
+console.log(linkedList.insert(30,30))
+linkedList.print();
+
+
+var reverseList = function(head) {
+    let prev = null;
+    let curr = head;
+    console.log(head,'1')
+    while(curr){
+        const nextNode = curr.next;
+        console.log(nextNode,'2')
+        curr.next = prev;
+        console.log(curr,'3')
+        prev = curr
+        console.log(prev,'4')
+        curr = nextNode;
+        console.log(curr,'5')
+    }
+    return prev;
+};
+
