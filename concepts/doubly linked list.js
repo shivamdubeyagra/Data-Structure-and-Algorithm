100dd74165e8656e87c0036013a64ff18fa37762
shivam-dubey-personal <shivamdubeyagra@gmail.com>
2025-12-02 18:20:07 +0530

    doubly linked list

---
+++ concepts/doubly linked list.js
@@ -0,0 +1,22 @@
+class ListNode {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+        this.prev = null;
+    }
+}
+function arrayToLList(arr) {
+    if (arr.length === 0) return null;
+    let head = new ListNode(arr[0]);
+    let current = head;
+    for (let i = 1; i < arr.length; i++) {
+        current.next = new ListNode(arr[i]);
+        current.next.prev = current;
+        current = current.next;
+    }
+    return head;
+}
+
+let arr = [10, 20, 30, 40,50,60];
+let list = arrayToLList(arr);
+console.log(list);
